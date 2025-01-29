c82ee83ba3a1831df44bad6b3d9fe28f05c01604
Petey von Ahn <vonahn2021@gmail.com>
2025-01-29 10:19:41 -0500

    Update lab 2 files with comments

--- expPoints.R
+++ expPoints.R
@@ -1,11 +1,11 @@
-source("runEpoch.R")
-expPoints <- function(down, toGo, toEndzone, fieldPos){
-  n <- 100
-  result <- seq(1:n)
-  for(i in 1:n){
-    result[i] <- runEpoch(down, toGo, toEndzone, fieldPos)
+source("runEpoch.R")     #Link file with function to run epoch
+expPoints <- function(down, toGo, toEndzone, fieldPos){   #Function to calculate expected points
+  n <- 100              #Set number of simulations
+  result <- seq(1:n)    #Create vector to store results in
+  for(i in 1:n){        #Loop over chosen length of simulation
+    result[i] <- runEpoch(down, toGo, toEndzone, fieldPos)    #Run epoch function and store in result vector
   }
-  mean(result)
+  mean(result)          #Find average result
 }
 
 expPoints(1, 10, 50, 50)
--- runEpoch.R
+++ runEpoch.R
@@ -1,21 +1,22 @@
-source("runDrive.R")
-runEpoch <- function(down, toGo, toEndzone, fieldPos){
-  team <- 1
-  toEndzone <- runDrive(1, 10, toEndzone, toEndzone)
-  count <- 0
-  while(toEndzone <= 100 & count != 10){
-    team <- team*-1
-    count <- count + 1
-    toEndzone <- runDrive(1, 10, toEndzone, toEndzone)
+source("runDrive.R")          #link file with function to run a drive
+runEpoch <- function(down, toGo, toEndzone, fieldPos){      #Epoch function
+  team <- 1               #Preset team variable as team 1
+  toEndzone <- runDrive(1, 10, toEndzone, toEndzone)    #Run drive function and store result
+  count <- 0              #Initialize count variable
+  max <- 10               #Define max # of drives before ending function
+  while(toEndzone <= 100 & count != max){    #Check there is no score and hasn't reached max drives
+    team <- team*-1       #Switch possession
+    count <- count + 1    #Increment count variable by 1
+    toEndzone <- runDrive(1, 10, toEndzone, toEndzone)    #Rerun drive function with new field position
   }
-  if(toEndzone > 110){
-    result <- 3*team
+  if(toEndzone > 110){    #Check if ball is past 110 yards (field goal)
+    result <- 3*team      #Assign 3 points multiplied by possession variable
   }
-  else if (toEndzone > 100){
-    result <- 7*team
+  else if (toEndzone > 100){    #Check if ball is between 100 and 110 yards (touchdown)
+    result <- 7*team            #Assign 7 points multipled by possession variable
   }
   else{
-    result <- 0
+    result <- 0           #Assign 0 points if reach max drives
   }
-  return(result)
+  result                  #Print result
 }
