0b00f02853428b06a1e7ad819b716072419dd5dc
pvonahn22 <hwf2js@virginia.edu>
2025-04-30 13:54:52 -0400

    Add files via upload

---
+++ final_run_play.R
@@ -0,0 +1,119 @@
+#samplingData <- readRDS('pbp2014-2024.rds')
+library(tidyverse)
+source('weather_yards_gained.R')
+
+# Choose weather conditions
+weather <- 'snowing'
+
+# Fit a logistic regression model for field goal attempts
+fgData <- samplingData %>% filter(play_type == 'field_goal')
+fgData$field_goal_result <- ifelse(fgData$field_goal_result == 'blocked', 'missed', fgData$field_goal_result)
+fgData$field_goal_factor <- as.numeric(factor(fgData$field_goal_result, levels = c("missed", "made"))) - 1
+fgLogisReg <- glm(field_goal_factor ~ yardline_100, data = fgData, family = binomial)
+
+
+library(nnet)
+
+# Fit a multinomial logistic regression model for fourth down choices
+fourthDownData <- samplingData %>% filter(down == 4 & play_type %in% c('punt', 'field_goal', 'pass', 'run'))
+fourthDownData$fourth_down_choice <- ifelse(fourthDownData$play_type %in% c('pass', 'run'), 'go_for_it', fourthDownData$play_type)
+fourthDownModel <- multinom(fourth_down_choice ~ yardline_100 + ydstogo, data = fourthDownData)
+
+# Function to simulate going for it on 4th down
+go_for_it <- function(down, ytg, newfp, ydsGained){
+  # If new field position is 0, it is a touchdown and exit to add points
+  if (newfp == 0){
+    list(D = 1, YTG = 0, FP = 105, exit_drive = 1)
+  }
+  # If they gain more yards than they need, return a first down with new field position
+  else if (ytg <= ydsGained){
+    list(D = 1, YTG = 10, FP = newfp, exit_drive = 0)
+  }
+  # If they don't gain enough yards, return a first down for the other team
+  else{
+    list(D = 1, YTG = 10, FP = 100-newfp, exit_drive = 1)
+  }
+}
+
+# Function to simulate a field goal attempt
+field_goal <- function(down, ytg, fp){
+  # If attempting field goal, use model to see
+  fgMakeRate <- predict(fgLogisReg, newdata = data.frame(yardline_100 = fp), type = "response")
+  result <- rbinom(1, 1, fgMakeRate)
+  # If they make the field goal, set field position to 115 and leave function
+  if (result == 1){
+    list(D = 1, YTG = 10, FP = 115, exit_drive = 1)
+  }
+  # If they miss the field goal, switch possessions and set new field position
+  else{
+    list(D = 1, YTG = 10, FP = 93 - fp, exit_drive = 1)
+  }
+}
+
+# Function to simulate punting
+punt <- function(down, ytg, fp){
+  puntDistance <- 30
+  list(D = 1, YTG = 10, FP = 100+puntDistance-fp, exit_drive = 1)
+}
+
+#Actual function to run each play
+run_play2 <- function(down, ytg, fp){
+  # Set turnover rate, mean and standard deviation for yards gained distribution
+  turnoverRate <- 0.05
+  ydMean <- 3
+  ydSD <- 2
+  # Sample to see if a turnover occurs
+  #check_turnover <- sample(c('yes', 'no'), 1, prob=c(turnoverRate, 1-turnoverRate))
+  check_turnover <- 'no'
+  if (check_turnover == 'yes'){
+    list(D = 1, YTG = 10, FP = 100-fp, exit_drive=1)
+  }
+  # No turnover
+  else{
+    # Sample from normal distribution with defined parameters for yards gained
+    # Round to make sure we only use whole numbers for yards gained
+    result <- getyg(39, 'sunny')
+    ydsGained <- as.numeric(result[1])
+    play_choice <- result[2]
+    # Set new field position based on yards gained
+    newfp <- fp - ydsGained
+    if (play_choice %in% c('Run Fumble', 'Pass Interception', 'Pass Fumble', 'QB Sack Fumble')){
+      list(D = 1, YTG = 10, FP = 100-fp, exit_drive=1)
+    }
+    # Check if it is 1st, 2nd, or 3rd down
+    if (down < 4){
+      # If new field position is 0, it is a touchdown and exit to add points
+      if (newfp == 0){
+        list(D = 1, YTG = 0, FP = 105, exit_drive = 1)
+      }
+      # If they gain more yards than they need, return a first down with new field position
+      else if (ytg <= ydsGained){
+        list(D = 1, YTG = 10, FP = newfp, exit_drive = 0)
+      }
+      # If they don't gain enough yards, return the next down with the new field position
+      # and new yards to gain but keep possession the same
+      else{
+        list(D = down+1, YTG = ytg-ydsGained, FP = newfp, exit_drive = 0)
+      }
+    }
+    
+    
+    # If 4th down, follow this progression
+    else{
+      # Predict probabilities for new data
+      probs <- predict(fourthDownModel, newdata = data.frame(yardline_100 = fp, ydstogo = ytg), type = "probs")
+      result <- sample(c('field goal', 'go for it', 'punt'), 1, prob = probs)
+      if (result == 'field goal'){
+        field_goal(down, ytg, fp)
+      }
+      else if (result == 'go for it'){
+        go_for_it(down, ytg, newfp, ydsGained)
+      }
+      else{
+        punt(down, ytg, fp)
+      }
+    }
+  }
+}
+
+run_play2(2, 10, 15)
